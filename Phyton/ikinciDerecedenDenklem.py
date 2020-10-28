a6e84a181f5627e67e4f22246d74684b3157ab67
huseyinozdem <huseyin_diyarbakir_21@hotmail.com>
2020-10-28 22:41:34 +0300

    update

--- Phyton/ikinciDerecedenDenklem.py
+++ Phyton/ikinciDerecedenDenklem.py
@@ -4,18 +4,18 @@ a = 1
 b = 8
 c = 7
 
-if a == 0 :
+if a == 0:
     print("Bu bir İkinci Dereceden denklem değildir")
 else:
     d = b * b - 4 * a * c
     if d < 0:
         print("Gerçel sayılarda kök yok.")
     if d == 0:
-        cakisikkök = -b / 2 * a
-        print("Çakışık iki kök vardır :", cakisikkök)
+        cakisikkok = -b / 2 * a
+        print("Çakışık iki kök vardır :", cakisikkok)
     if d > 0:
-        birincikök = (-b - math.sqrt(d)) / 2 * a
-        ikincikök = (-b + math.sqrt(d)) / 2 * a
+        birincikok = (-b - math.sqrt(d)) / 2 * a
+        ikincikok = (-b + math.sqrt(d)) / 2 * a
         print("İki kök vardır")
-        print("Birinci kök :", birincikök)
-        print("İkinci kök :", ikincikök)
+        print("Birinci kök :", birincikok)
+        print("İkinci kök :", ikincikok)

b7b59631ce033388bddc2d3932e5988190ee07cd
huseyinozdem <huseyin_diyarbakir_21@hotmail.com>
2020-11-07 20:18:36 +0300

    update

--- Phyton/altprg.py
+++ Phyton/altprg.py
@@ -1,5 +1,6 @@
 
-
+def yuvarlma(s):
+    return round(s+0.0000000001)
 
 def odevHesaplama(v):
     odev= 50+ v/2
@@ -12,6 +13,18 @@ odev = odevHesaplama(vize)
 final = 45
 print('Ödev :', odev)
 print('Vize :', vize)
+
 print('Final :', final)
 
+ortalama = ortHesapla(vize,odev,final)
+print('Ortalama :',ortalama)
+
+print()
+ortY = round(ortalama)
+print('Yuvarlanmış Ortalama :', ortY)
+
+ortY2 = yuvarlma(ortalama)
+
+print()
+print('Doğru Yuuvarlanmış Ortalama :', ortY2)
 

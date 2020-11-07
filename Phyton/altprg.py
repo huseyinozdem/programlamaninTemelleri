c517eff96367f099af4de6b85dcc4a25ba390d10
huseyinozdem <huseyin_diyarbakir_21@hotmail.com>
2020-11-07 19:52:38 +0300

    update

---
+++ Phyton/altprg.py
@@ -0,0 +1,17 @@
+
+
+
+def odevHesaplama(v):
+    odev= 50+ v/2
+    return odev
+def ortHesapla(v , o , f):
+    return v*0.3 + o *0.2 + f*0.5
+
+vize = 60
+odev = odevHesaplama(vize)
+final = 45
+print('Ödev :', odev)
+print('Vize :', vize)
+print('Final :', final)
+
+

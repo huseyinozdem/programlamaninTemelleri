5c0cad1f772e085cc8af34627bc4faf98857bf55
huseyinozdem <huseyin_diyarbakir_21@hotmail.com>
2021-01-16 21:43:52 +0300

    update

--- Phyton/sinavKontrol.py
+++ Phyton/sinavKontrol.py
@@ -1,4 +1,9 @@
+import sys
+import os
 import random
+klasorAdi = os.path.dirname(sys.argv[0])
+dosyaIsmi = klasorAdi + "/test.txt"
+
 soruSayisi = 40
 ogrenciSayisi = 60
 d = {}
@@ -42,19 +47,52 @@ def sinavDegerlendir():
           yalisSayisi[i] += 1
     bosSayisi[i] = soruSayisi - (dogruSayisi[i] + yalisSayisi[i])
     puan[i] = soruBasinaDusenPuan * dogruSayisi[i]
+
+
+def sinavSirala():
+  for i in range(1, ogrenciSayisi):
+    for j in range(i + 1, ogrenciSayisi + 1):
+      if puan[i] < puan[j]:
+        for k in range(1, soruSayisi + 1):
+          g = d[i, k]
+          d[i, k] = d[j, k]
+          d[j, k] = g
+
+        g = dogruSayisi[i] ; dogruSayisi[i] = dogruSayisi[j] ; dogruSayisi[j] = g
+        g = yalisSayisi[i] ; yalisSayisi[i] = yalisSayisi[j] ; yalisSayisi[j] = g
+        g = bosSayisi[i] ; bosSayisi[i] = bosSayisi[j] ; bosSayisi[j] = g
+        g = puan[i] ; puan[i] = puan[j] ; puan[j] = g
+
+def sinavYaz():
+  dosya = open(dosyaIsmi, "w")
+  s = '    '
   for j in range(1, soruSayisi + 1):
-    print(d[0, j], end="")
-  print()
+    s += d[0 ,j]
+  print(s, file=dosya)
+
   for i in range(1, ogrenciSayisi + 1):
+    s = '%3d.' % i
     for j in range(1, soruSayisi + 1):
-      print(d[i, j], end="")
-    print()
+      s += d[i, j]
 
+    s += ' ** Doğru Sayısı:%3d Yanlış Sayısı:%3d Boş Sayısı:%3d Puan:%6.2f' %\
+         (dogruSayisi[i], yalisSayisi[i], bosSayisi[i], puan[i])
+    print(s, file=dosya)
+  dosya.close()
 
+def sinavOku():
+  if os.path.isfile(dosyaIsmi)==False:
+    print("dosya diskte mevcut değil")
+  else:
+    dosya = open(dosyaIsmi, "r")
+    for s in dosya:
+      print(s, end="")
 
-
+    dosya.close()
 
 
 sinavHazirla()
 sinavDegerlendir()
-
+sinavSirala()
+sinavYaz()
+sinavOku()
